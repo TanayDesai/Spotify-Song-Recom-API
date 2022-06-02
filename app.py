400c8b0fdda600a4d712edfbf68c622785879ccb
Tanay Desai <tanaydesai5@example.com>
2022-06-02 22:43:50 +0530

    V22

--- app.py
+++ app.py
@@ -12,11 +12,10 @@ def home():
 @app.route("/song",methods=['GET'])
 def call():
     name = str(request.args["name"])
-    try:
-        getsongs = GetSongs(name)
-        result = getsongs.getRecom()
-        return jsonify({'result':result})
-    except:
-        result = "Error, Try again."
-        return jsonify({'result':result})
+    getsongs = GetSongs(name)
+    result = getsongs.getRecom()
+
+    
+    return jsonify({'result':result})
+
 
